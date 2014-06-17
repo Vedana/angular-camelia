8cb68a0a705cbf013fdf165710c5b21796b97cfc
oeuillot <olivier.oeuillot@gmail.com>
2014-06-17 10:39:51 +0200

    Add template feature

---
+++ src/scripts/components/template.js
@@ -0,0 +1,69 @@
+/**
+ * @product CameliaJS (c) 2014 Vedana http://www.vedana.com
+ * @license Creative Commons - The licensor permits others to copy, distribute,
+ *          display, and perform the work. In return, licenses may not use the
+ *          work for commercial purposes -- unless they get the licensor's
+ *          permission.
+ * @author olivier.oeuillot@vedana.com
+ */
+
+(function(window, angular, undefined) {
+	'use strict';
+
+	var module = angular.module("camelia.components.template", [ "camelia.core" ]);
+
+	module.factory("camelia.components.Template", [ "$log",
+		"$timeout",
+		"$exceptionHandler",
+		"$compile",
+		"camelia.core",
+		function($log, $timeout, $exceptionHandler, $compile, cc) {
+
+			var anonymousId = 0;
+
+			/*
+			 * ------------------------ Popup --------------------------
+			 */
+
+			var Template = function($scope, element, containerScope, transcludeFunc) {
+				this.$scope = $scope;
+				this._transcludeFunc = transcludeFunc;
+				this.id = "template_" + (anonymousId++);
+				// element.data("cm_component", this);
+
+				if (!containerScope.templates) {
+					containerScope.templates = [];
+				}
+
+				containerScope.templates.push(this);
+			}
+
+			Template.markContainer = function($scope, element) {
+				element.data('$cmTemplateContainerController', $scope);
+			}
+
+			Template.prototype = {
+
+				/**
+				 * @returns {Promise}
+				 */
+				transclude: function(parent, $scope) {
+
+					var f = this._transcludeFunc;
+
+					var clone = new f($scope, function(clone, newScope) {
+						
+						//clone.scope=newScope;
+						
+						parent.append(clone);
+					});
+
+					return clone;
+				}
+			};
+
+			return Template;
+
+		} ]);
+
+})(window, window.angular);
---
+++ src/scripts/directives/template.js
@@ -0,0 +1,42 @@
+/**
+ * @product CameliaJS (c) 2014 Vedana http://www.vedana.com
+ * @license Creative Commons - The licensor permits others to copy, distribute,
+ *          display, and perform the work. In return, licenses may not use the
+ *          work for commercial purposes -- unless they get the licensor's
+ *          permission.
+ * @author olivier.oeuillot@vedana.com
+ */
+
+(function(window, angular, undefined) {
+	'use strict';
+
+	var module = angular.module('camelia.directives.template', [ 'camelia.components.template' ]);
+
+	module.directive("cmTemplate", [ "$log", "camelia.components.Template", function($log, Template) {
+
+		return {
+			require: "^cmTemplateContainer",
+			restrict: "E",
+			scope: {
+				name: '@'
+				//enabled: '@'
+			},
+			transclude: true,
+
+			compile: function() {
+				return {
+					pre: function($scope, element, attrs, controller) {
+
+						$scope.enabledExpresion = element.attr("enabled");
+					},
+					post: function($scope, element, attrs, controller, transcludeFunc) {
+
+						var template = new Template($scope, element, controller, transcludeFunc);
+
+						element.remove();
+					}
+				};
+			}
+		};
+	} ]);
+})(window, window.angular);
