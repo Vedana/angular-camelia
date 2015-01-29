666a8bf44f95401ca7ff2b3fe59be3b42dcda4f8
oeuillot <olivier.oeuillot@gmail.com>
2015-01-29 15:45:47 +0100

    Use scopedWrapper Add support of waiting and error pages

---
+++ src/scripts/scopeWrapper.js
@@ -0,0 +1,40 @@
+/**
+ * @product CameliaJS (c) 2014 Vedana http://www.vedana.com
+ * @license Creative Commons - The licensor permits others to copy, distribute,
+ *          display, and perform the work. In return, licenses may not use the
+ *          work for commercial purposes -- unless they get the licensor's
+ *          permission.
+ * @author olivier.oeuillot@vedana.com
+ */
+
+(function(window, angular, undefined) {
+	'use strict';
+
+	var LINK_SCOPE = true;
+
+	var module = angular.module('camelia.scopeWrapper', [ "camelia.core" ]);
+
+	module.factory('camelia.ScopeWrapper', [ "$rootScope", "camelia.core", function($rootScope, cc) {
+		function ScopeWrapper($scope) {
+			this.$scope = $scope;
+
+			if (LINK_SCOPE) {
+				$scope.$cmLink = this;
+			}
+		}
+
+		[ '$destroy', '$emit', '$broadcast', '$on', '$eval', '$evalSync', '$digest' ].forEach(function(name) {
+			ScopeWrapper.prototype[name] = function() {
+				var scope = this.$scope;
+				return scope[name].apply(scope, arguments);
+			};
+		});
+
+		ScopeWrapper.prototype.toString = function() {
+			return "[Scoped object $id=" + this.$scope.$id + "]";
+		};
+
+		return ScopeWrapper;
+
+	} ]);
+})(window, window.angular);
